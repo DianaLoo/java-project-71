ff96d33d37d6242101f4f0728b0b6ee9205beeb9
Diana Lomakina <diana_st16@mail.ru>
2024-12-17 17:23:44 +0300

    add DifferTest.java

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("application")
     id("checkstyle")
+    jacoco
 }
 
 group = "hexlet.code"
@@ -16,9 +17,8 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation ("info.picocli:picocli:4.7.6")
-    implementation ("com.fasterxml.jackson.core:jackson-core:2.17.2")
-    implementation ("com.fasterxml.jackson.core:jackson-core:2.17.2")
-    implementation ("com.fasterxml.jackson.core:jackson-annotations:2.17.2")
+    //implementation ("com.fasterxml.jackson.core:jackson-core:2.17.2")
+    //implementation ("com.fasterxml.jackson.core:jackson-annotations:2.17.2")
     implementation ("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.17.2")
 
 
@@ -26,4 +26,8 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.jacocoTestReport {
+    reports { xml.required.set(true) }
 }
