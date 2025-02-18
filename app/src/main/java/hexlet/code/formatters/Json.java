747175909053faf6f55e358046fffd7bc8e7e5a1
Diana Lomakina <diana_st16@mail.ru>
2025-02-18 10:48:40 +0300

    changed

--- app/src/main/java/hexlet/code/formatters/Json.java
+++ app/src/main/java/hexlet/code/formatters/Json.java
@@ -2,6 +2,7 @@ package hexlet.code.formatters;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
 import java.util.List;
@@ -10,8 +11,8 @@ import java.util.Map;
 public class Json {
     public static String formatJson(List<Map<String, Object>> file) throws JsonProcessingException {
         try {
-            ObjectMapper mapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
-            return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(file);
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.writeValueAsString(file);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
