d2fe75d305859f88fc4c245966e294a8ce63edca
Diana Lomakina <diana_st16@mail.ru>
2025-02-19 14:48:04 +0300

    changed

--- app/src/main/java/hexlet/code/formatters/Json.java
+++ app/src/main/java/hexlet/code/formatters/Json.java
@@ -1,13 +1,7 @@
 package hexlet.code.formatters;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-
 import java.util.List;
-import java.util.Map;
-
 
 public class Json {
     public static String formatJson(List file) throws Exception {
